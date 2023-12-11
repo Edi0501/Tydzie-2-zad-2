d254e90b9539b03a21f10df610e68ea09a4224ff
Edi0501 <edytanowakowska05@gmail.com>
2023-12-11 12:12:36 +0100

    Add project files.

---
+++ Tydzień 2 zad 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Tydzien2zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            {
+                char zmienna1 = 'a';
+                char zmienna2 = '1';
+                char zmienna3 = '3';
+
+                Console.WriteLine($"{zmienna1 = '3'},{zmienna2 = '1' }, {zmienna3 = 'a'}");
+            }
+        }
+
+    }
+}
